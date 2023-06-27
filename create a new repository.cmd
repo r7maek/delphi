290e1596c08440ee01847b29ef1dfa1b24e5ec6f
r7maek <r7maek@gmail.com>
2023-06-27 04:06:27 +0100

    first commit

--- create a new repository.cmd
+++ create a new repository.cmd
@@ -1,7 +1,7 @@
 echo "# delphi" >> README.md
 git init
 git add README.md
-git add *
+git add *.*
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/r7maek/delphi.git
